952ec6971e162f999d00e2c2499be7e07aabbdec
Michael Ottinger <michael@ottinger.net>
2019-05-11 01:37:13 -0400

    Add rudimentary JSON api functionality (stop names/times)

--- eventproxy.js
+++ eventproxy.js
@@ -19,12 +19,35 @@ app.get('/eta', function(req,res) {
 
 });
 
+var currentStopTimes = {};
+var currentStops = {};
+var currentRoutes = {};
+app.get('/api/:stopId', function(req,res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(currentStops[req.params.stopId]);
+});
+
 var es = new EventSource("https://stream.transit.usft.com/eta");
 es.onmessage = function(e) {
+	// Rebroadcast data to clients
 	for(let r of clients) {
 		r.write("data: " + e.data + "\n\n"); // needs TWO newlines for browser to see it!
 	}
-	console.log(e.data);
+
+	// Load it into our arrays for our use
+	var m = JSON.parse(e.data);
+	// fill stop information
+	if(!currentStops[m.stopId])
+		currentStops[m.stopId] = {name: m.stopName};
+	if(!currentStops[m.stopId][m.routeId])
+		currentStops[m.stopId][m.routeId] = {name: m.routeName,
+			stopId: m.stopId,
+			times: [m.firstDuration, m.secondDuration, m.thirdDuration]};
+	else
+		currentStops[m.stopId][m.routeId].times = [m.firstDuration, m.secondDuration, m.thirdDuration];
+
+
+	console.log(m);
 }
 
 
